df056d4b75306f325f7c2dbccbf967515acfa8c4
depstor <depstor90@gmail.com>
2025-06-14 16:02:12 +0300

    swiper-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,28 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "/src/sass/style.scss";
 import 'purecss/build/grids-min.css';
 import 'purecss/build/grids-responsive-min.css';
 
 
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 10,
+    breakpoints: {
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        1920: {
+            slidesPerView: 5,
+            spaceBetween: 35
+        },
+    }
+});
