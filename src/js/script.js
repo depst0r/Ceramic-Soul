7f378b6f3c7ef0651575fc039c000685db6ae0f7
depstor <depstor90@gmail.com>
2025-06-14 16:35:50 +0300

    skide-adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -9,28 +9,32 @@ import "/src/sass/style.scss";
 import 'purecss/build/grids-min.css';
 import 'purecss/build/grids-responsive-min.css';
 
+try {
+    new Swiper('.works__slider', {
+        modules: [Navigation, Pagination],
+        slidesPerView: 1,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        loop: true,
+        spaceBetween: 10,
+        breakpoints: {
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5
+            },
+            1920: {
+                slidesPerView: 5,
+                spaceBetween: 35
+            },
+        },
+    });
+} catch (error) {
+    console.log(error)
+}
 
-const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination],
-    slidesPerView: 1,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    loop: true,
-    spaceBetween: 10,
-    breakpoints: {
-        1200: {
-            slidesPerView: 3,
-            spaceBetween: 5
-        },
-        1920: {
-            slidesPerView: 5,
-            spaceBetween: 35
-        },
-    },
-});
